53db0a4f078f653c3ae3342f97d49331760954e9
Ryan Schmelter <rschmelter33@gmail.com>
2017-12-08 21:21:09 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
   end
 
   def reverse_transfer
-  if @status = "completed"
+  if @status = "complete"
     @sender.balance += @amount
     @receiver.balance -= @amount
     @status = "reversed"
