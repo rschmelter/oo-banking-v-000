f62dabd1acf6be0019ce7cb5de01866cd2311636
Ryan Schmelter <rschmelter33@gmail.com>
2017-12-08 19:04:12 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,8 @@
 class BankAccount
-  attr_accessor :account
+  attr_accessor :account, :name
 
-  def initialize(account)
+  def initialize(account, name)
     @account = account
+    @name = name
   end
 end
